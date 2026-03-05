ba97f7150ecfd440c297699350a1c871218609da
Sam Stille <samstille2712@yahoo.nl>
2026-03-05 14:26:58 +0100

    gunicorn added voor meerdere gebruikers op site

--- app/web.py
+++ app/web.py
@@ -479,4 +479,4 @@ document.addEventListener('keydown', e => {
 '''
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=os.environ.get('DEBUG', 'false') == 'true')
